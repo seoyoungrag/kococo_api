ab480307106670432e80b94a3c83347b58bac487
yr.seo <yr.seo@dwebss.co.kr>
2019-05-23 22:36:46 +0900

    빌드 자동화 진행 중

--- src/main/java/kr/co/dwebss/kococo/api/KococoApplication.java
+++ src/main/java/kr/co/dwebss/kococo/api/KococoApplication.java
@@ -2,10 +2,16 @@ package kr.co.dwebss.kococo.api;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
 
+@EnableJpaAuditing
 @SpringBootApplication
 public class KococoApplication {
 
+    public static final String APPLICATION_LOCATIONS = "spring.config.location="
+            + "classpath:application.yml,"
+            + "/app/config/kococo_api/real-application.yml";
+    
 	public static void main(String[] args) {
 		SpringApplication.run(KococoApplication.class, args);
 	}
