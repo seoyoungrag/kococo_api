41601d1f22dc5643af617f63da5a00f923c018c5
yr.seo <yr.seo@dwebss.co.kr>
2019-05-28 15:06:00 +0900

    코드 테이블 조회 추가

--- src/main/java/kr/co/dwebss/kococo/api/entities/repository/CodeRepository.java
+++ src/main/java/kr/co/dwebss/kococo/api/entities/repository/CodeRepository.java
@@ -1,10 +1,14 @@
 package kr.co.dwebss.kococo.api.entities.repository;
 
+import java.util.List;
+
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.repository.query.Param;
 import org.springframework.data.rest.core.annotation.RepositoryRestResource;
 
 import kr.co.dwebss.kococo.api.entities.Code;
 
 @RepositoryRestResource(collectionResourceRel = "code", path = "code")
 public interface CodeRepository extends JpaRepository<Code,Integer>{
+    List<Code> findByCodeBetween(@Param("startCd") int startCd, @Param("endCd") int endCd);
 }
