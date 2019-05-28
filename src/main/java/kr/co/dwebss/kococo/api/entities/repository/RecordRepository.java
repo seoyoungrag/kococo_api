5faacbfcc5c1ae99bc401aa850632807825a7d91
yr.seo <yr.seo@dwebss.co.kr>
2019-05-28 10:07:18 +0900

    레코드 테이블에서 userappid로 레코드 조회

--- src/main/java/kr/co/dwebss/kococo/api/entities/repository/RecordRepository.java
+++ src/main/java/kr/co/dwebss/kococo/api/entities/repository/RecordRepository.java
@@ -1,10 +1,14 @@
 package kr.co.dwebss.kococo.api.entities.repository;
 
+import java.util.List;
+
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.repository.query.Param;
 import org.springframework.data.rest.core.annotation.RepositoryRestResource;
 
 import kr.co.dwebss.kococo.api.entities.Record;
 
 @RepositoryRestResource(collectionResourceRel = "record", path = "record")
 public interface RecordRepository extends JpaRepository<Record,Integer>{
+    List findByUserAppId(@Param("userAppId") String userAppId);
 }
